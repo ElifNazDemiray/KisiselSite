c735064dcb572b93e20de75a0243eee39e657b06
elif <elif@DESKTOP-U2NKEJ5>
2026-02-22 22:52:04 +0300

    Add project files.

---
+++ KisiselSite/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class HomeController : Controller
+{
+    public IActionResult Index() => View();
+    public IActionResult Hakkimda() => View();
+    public IActionResult Yeteneklerim() => View();
+    public IActionResult Projelerim() => View();
+    public IActionResult Deneyimlerim() => View();
+    public IActionResult Iletisim() => View();
+}
